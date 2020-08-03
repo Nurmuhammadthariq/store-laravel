177eaf4820c9c8962ac9e29c2c0695c1c2935f37
thoriqnoer <thoriq.noer@gmail.com>
2020-08-04 06:10:23 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/details', 'DetailController@index')->name('detail');
+Route::get('/cart', 'CartController@index')->name('cart');
+Route::get('/success', 'CartController@success')->name('success');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register/success', 'Auth\RegisterController@success')->name('Register-success');
+
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('dashboard/products', 'DashboardProductController@index')->name('dashboard-product');
+
+Auth::routes();
+
+
