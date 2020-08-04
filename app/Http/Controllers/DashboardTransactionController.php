8555498ca77cc7624c0f6a00acc37813eb81b24b
thoriqnoer <thoriq.noer@gmail.com>
2020-08-04 10:51:40 +0700

    Create page dashboard transaction in DashboardTransactionController

---
+++ app/Http/Controllers/DashboardTransactionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardTransactionController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('pages.dashboard-transactions');
+    }
+
+    public function details()
+    {
+        $status = 'SHIPPING';
+        return view('pages.dashboard-transactions-details', [
+            'status' => $status
+        ]);
+    }
+}
