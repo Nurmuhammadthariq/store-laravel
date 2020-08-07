21bec2b3a56eb5f153341d84f7caf4d46220845f
thoriqnoer <thoriq.noer@gmail.com>
2020-08-07 10:31:20 +0700

    Modif Controller CategoryController ( make CRUD )

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+use App\Http\Requests\Admin\CategoryRequest;
+
+use Yajra\DataTables\Facades\DataTables;
 
 class CategoryController extends Controller
 {
@@ -14,6 +21,45 @@ class CategoryController extends Controller
      */
     public function index()
     {
+		if(request()->ajax()) 
+		{
+			$query = Category::query();
+
+			return Datatables::of($query)
+				->addColumn('action', function($item){
+					return '
+						<div class="btn-group">
+							<div class="dropdown">
+								<button class="btn btn-primary dropdown-toggle mr-1 mb-1" 
+									type="button"
+									data-toggle="dropdown">
+									Aksi
+								</button>
+								<div class="dropdown-menu">
+									<a class="dropdown-item" href="' . route('category.edit', $item->id) . '">
+										Ubah
+									</a>
+									<form action="' . route('category.destroy', $item->id) . '" 
+										method="POST">
+										' . method_field('delete') . csrf_field() . '
+										<button type="submit" class="dropdown-item text-danger">
+											Hapus
+										</button>
+									</form>
+								</div>
+							</div>
+						</div>
+					';
+				})
+				->editColumn('photo', function($item) {
+					return $item->photo ? '<img src="'. Storage::url($item->photo) .'"	style="max-height: 40px;" />' : '';
+				})
+				->rawColumns(['action','photo'])
+				->setRowClass(function ($item){
+					return $item->id % 2 == 0 ? 'alert-success' : 'alert-warning';
+				})
+				->make();
+		}
         return view('pages.admin.category.index');
     }
 
@@ -24,7 +70,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.category.create');
     }
 
     /**
@@ -33,9 +79,16 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-        //
+		$data = $request->all();
+		
+		$data['slug'] = Str::slug($request->name);
+		$data['photo'] = $request->file('photo')->store('assets/category', 'public');
+
+		Category::create($data);
+
+		return redirect()->route('category.index');
     }
 
     /**
@@ -56,8 +109,12 @@ class CategoryController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
-    {
-        //
+    {	
+		$item = Category::findOrFail($id);
+		
+        return view('pages.admin.category.edit', [
+			'item' => $item
+		]);
     }
 
     /**
@@ -67,9 +124,18 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CategoryRequest $request, $id)
     {
-        //
+        $data = $request->all();
+		
+		$data['slug'] = Str::slug($request->name);
+		$data['photo'] = $request->file('photo')->store('assets/category', 'public');
+
+		$item = Category::findOrFail($id);
+
+		$item->update($data);
+
+		return redirect()->route('category.index');
     }
 
     /**
@@ -80,6 +146,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+		$item = Category::findOrFail($id);
+		$item->delete();
+
+		return redirect()->route('category.index');
     }
 }
